0985148e61796a2c9d950dae1a20e5ebc1442edd
Christopher West <9ninemind@gmail.com>
2015-04-16 15:51:24 -0600

    adds note type interface

---
+++ Model/NoteTypeInterface.php
@@ -0,0 +1,9 @@
+<?php
+namespace BlackBoxCode\Pando\Bundle\NoteBundle\Model;
+
+use BlackBoxCode\Pando\Bundle\BaseBundle\Model\TypeInterface;
+
+interface NoteTypeInterface extends TypeInterface
+{
+
+}
